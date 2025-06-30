582eeed7513178cfa1b08ebc0b3db1a13c3d7b93
SURAJS2810 <surajsundarraju2001@gmail.com>
2025-06-30 18:13:29 +0530

    Delete Arrays directory

--- Arrays/Ipl.java
+++
@@ -1,6 +0,0 @@
-class Ipl{
-static String teamnames[]={"RCB","CSK","MI","GT","LSG","KKR","SRH","PBKS","RR","DC"};
-public static void main(String[] ipl){
-System.out.println(teamnames[0]+","+teamnames[1]+","+teamnames[2]+","+teamnames[3]+","+teamnames[4]+","+teamnames[5]+","+teamnames[6]+","+teamnames[7]+","+teamnames[8]+","+teamnames[9]);
-}
-}
--- Arrays/PeriodicTable.java
+++
@@ -1,6 +0,0 @@
-class PeriodicTable {
-static String elements[] = {"Hydrogen (H)","Helium (He)","Lithium (Li)","Beryllium (Be)","Boron (B)","Carbon (C)","Nitrogen (N)","Oxygen (O)","Fluorine (F)","Neon (Ne)","Sodium (Na)","Magnesium (Mg)"};
-public static void main(String[] periodic) {
-System.out.println(elements[0]  + ", " + elements[1]  + ", " +elements[2]  + ", " + elements[3]  + ", " +elements[4]  + ", " + elements[5]  + ", " +elements[6]  + ", " + elements[7]  + ", " +elements[8]  + ", " + elements[9]  + ", " +elements[10] + ", " + elements[11]);
-    }
-}
