759c12eb6e6ddc64502cab4a3811c89a2c076d50
SURAJS2810 <surajsundarraju2001@gmail.com>
2025-06-30 18:17:34 +0530

    Delete Arrays directory

--- Arrays/Hospital.java
+++
@@ -1,8 +0,0 @@
-class Hospital {
-static String cardiologists[] = {"Dr. Ashok Seth","Dr. Devi Prasad Shetty","Dr. Naresh Trehan"};
-static String specialists[] = {"Dr. Priyadarshi Ranjan","Dr. Randeep Guleria","Dr. D. Nageshwar Reddy","Dr. Abhideep Chaudhary","Dr. Subhash Gupta","Dr. Naresh Trehan Jr.","Dr. Deepak Namjoshi"};
-public static void main(String[] hospital) {
-System.out.println(cardiologists[0] + ", " + cardiologists[1] + ", " + cardiologists[2]);
-System.out.println(specialists[0] + ", " + specialists[1] + ", " + specialists[2] + ", " +specialists[3] + ", " + specialists[4] + ", " + specialists[5] + ", " + specialists[6]);
-    }
-}
